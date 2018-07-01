666a1c82cef674bc06380ed28735a5bfd168246a
David McGaughey <mcgaughey@gmail.com>
2018-07-01 13:53:36 -0400

    removing cruft, adding custon name field, set input as.character

--- R/link_generator.R
+++ R/link_generator.R
@@ -1,54 +1,42 @@
 #' Link generator for See GEM interactive report
 #'
-#' Takes a base url and a column from a data frame to generate a vector of URLs
+#' Takes a base url and a column from a data frame to generate a vector of URLs. 
+#' You must use html format to specify special characters and spaces. 
+#' Used in See_GEM_formatter function to format specified colums. 
 #'
 #'@param base_url The consistent base URL 
 #'@param ID The column ID to build the unique part of the URL
+#'@param split_on What delimiter to split the field on? Defaults to comma.
+#'@param link_name What to use for the link name? Defaults to using the row value of the column
 #'
 #'@return None
 #'
 #'@examples
-#' link_generator()
+#' GEMINI_data$rs_ids <- sapply(gem_view$rs_ids, 
+#' function(x) 
+#' link_generator('https://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs=', as.character(x)) )
 
 
-link_generator <- function(base_url, ID){
+link_generator <- function(base_url, ID, split_on=',', link_name = NA){
+  ID <- as.character(ID)
+  link_name = as.character(ID)
   if (is.na(ID) | is.na(base_url) | ID == 'None' | base_url == 'None'){
     url <- NA
   }
   else {
-    ends <- strsplit(ID, ',')[[1]]
+    ends <- strsplit(ID, split_on)[[1]]
     url <- ''
     for (end in ends){
-      url <- paste(paste0('<a href="', base_url, end, '" target="_blank">', end, '</a> '), url)
+      if (is.na(link_name)){
+        url <- paste(paste0('<a href="', base_url, end, '" target="_blank">', end, '</a> '), url)
+      }
+      else{
+        url <- paste(paste0('<a href="', base_url, end, '" target="_blank">', link_name, '</a> '), url)
+      }
     }
     if (ID == 'None') {
       url <- NA
     }
   }
   return(as.character(url))
-}
-clinvar_link_generator <- function(base_url, ID, clinvar_sig){
-  if (clinvar_sig == 'None' | is.na(clinvar_sig) | is.na(ID) | ID == 'None') {
-    url <- NA
-  }
-  else {
-    ends <- strsplit(ID, ',')[[1]]
-    url <- ''
-    for (end in ends){
-      url <- paste(paste0('<a href="', base_url, end, '" target="_blank">', as.character(clinvar_sig), '</a> '), url)
-    }
-  }
-  return(as.character(url))
-}
-google_link_generator <- function(var, gene){
-  if (is.na(var) || is.na(gene) || var == 'None' || gene == 'None'){
-    output <- NA
-  }
-  else {
-    url = 'https://scholar.google.com/scholar?hl=en&q='
-    var <- gsub('>','%3E',var)
-    var <- strsplit(var, ':')[[1]][2]
-    output <- paste0('<a href="', url, var, '+', gene, '" target="_blank">', 'Feeling&nbspLucky?', '</a>')
-  }
-  return(output)
 }
