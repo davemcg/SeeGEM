52f5d271a730b9b535394d48cef41e152cae6a47
David McGaughey <mcgaughey@gmail.com>
2018-07-03 16:49:24 -0400

    print progress, do fam filter for ACMG

--- inst/example_scripts/GEMINI_queries.R
+++ inst/example_scripts/GEMINI_queries.R
@@ -17,7 +17,7 @@ GEMINI_list$ar <- gemini_test_wrapper(gemini_db,
                     (is_coding=1 OR is_splicing=1 OR impact_severity='HIGH') 
                     AND filter IS NULL",
                                       families = family_name)
-
+print('Autosomal Recessive test done')
 GEMINI_list$ad <- gemini_test_wrapper(gemini_db, 
                                       test = 'autosomal_dominant', 
                                       min_gq = 20, 
@@ -26,7 +26,7 @@ GEMINI_list$ad <- gemini_test_wrapper(gemini_db,
                                       (is_coding=1 OR is_splicing=1 OR impact_severity='HIGH') 
                                       AND filter IS NULL",
                                       families = family_name)
-
+print('Autosomal Dominant test done')
 GEMINI_list$dn <- gemini_test_wrapper(gemini_db, 
                                       test = 'de_novo', 
                                       min_gq = 20, 
@@ -35,7 +35,7 @@ GEMINI_list$dn <- gemini_test_wrapper(gemini_db,
                                       (is_coding=1 OR is_splicing=1 OR impact_severity='HIGH') 
                                       AND filter IS NULL",
                                       families = family_name)
-
+print('De novo test done')
 GEMINI_list$xlr <- gemini_test_wrapper(gemini_db, 
                                        test = 'x_linked_recessive', 
                                        min_gq = 20, 
@@ -44,7 +44,7 @@ GEMINI_list$xlr <- gemini_test_wrapper(gemini_db,
                                        (is_coding=1 OR is_splicing=1 OR impact_severity='HIGH') 
                                        AND filter IS NULL",
                                        families = family_name)
-
+print('XLR test done')
 GEMINI_list$xld <- gemini_test_wrapper(gemini_db, 
                                        test = 'x_linked_dominant', 
                                        min_gq = 20, 
@@ -53,7 +53,7 @@ GEMINI_list$xld <- gemini_test_wrapper(gemini_db,
                                        (is_coding=1 OR is_splicing=1 OR impact_severity='HIGH') 
                                        AND filter IS NULL",
                                        families = family_name)
-
+print('XLD test done')
 GEMINI_list$xldn <- gemini_test_wrapper(gemini_db, 
                                         test = 'x_linked_de_novo', 
                                         min_gq = 20, 
@@ -62,7 +62,7 @@ GEMINI_list$xldn <- gemini_test_wrapper(gemini_db,
                                        (is_coding=1 OR is_splicing=1 OR impact_severity='HIGH') 
                                        AND filter IS NULL",
                                         families = family_name)
-
+print('XL De Novo test done')
 GEMINI_list$me <- gemini_test_wrapper(gemini_db, 
                                       test = 'mendel_errors', 
                                       min_gq = 20, 
@@ -71,7 +71,7 @@ GEMINI_list$me <- gemini_test_wrapper(gemini_db,
                                         (is_coding=1 OR is_splicing=1 OR impact_severity='HIGH') 
                                         AND filter IS NULL",
                                       families = family_name)
-
+print('Mendelian Errors test done')
 GEMINI_list$ch <- gemini_test_wrapper(gemini_db, 
                                       test = 'comp_hets', 
                                       min_gq = 20, 
@@ -80,7 +80,7 @@ GEMINI_list$ch <- gemini_test_wrapper(gemini_db,
                                       (is_coding=1 OR is_splicing=1 OR impact_severity='HIGH') 
                                       AND filter IS NULL",
                                       families = family_name)
-
+print('Compound Hets test done')
 acmg_genes = c('ACTA2','ACTC1','APC','APOB','ATP7B','BMPR1A','BRCA1','BRCA2',
                'CACNA1S','COL3A1','DSC2','DSG2','DSP','FBN1','GLA','KCNH2','KCNQ1',
                'LDLR','LMNA','MEN1','MLH1','MSH2','MSH6','MUTYH','MYBPC3','MYH11',
@@ -88,19 +88,20 @@ acmg_genes = c('ACTA2','ACTC1','APC','APOB','ATP7B','BMPR1A','BRCA1','BRCA2',
                'PTEN','RB1','RET','RYR1','RYR2','SCN5A','SDHAF2','SDHB','SDHC',
                'SDHD','SMAD3','SMAD4','STK11','TGFBR1','TGFBR2','TMEM43','TNNI3',
                'TNNT2','TP53','TPM1','TSC1','TSC2','VHL','WT1')
-
+print('ACMG test done')
 GEMINI_list$acmg <- gemini_query_wrapper(gemini_db,
                                          ... = paste0("\"SELECT * FROM variants WHERE (gene IN (\'",
                                                       paste(acmg_genes, collapse="\',\'"),
                                                       "\')) AND ((clinvar_sig LIKE '%pathogenic%' OR impact_severity='HIGH') 
-                                                     AND (aaf < 0.1 AND aaf_esp_all < 0.01 AND
-                                                     aaf_1kg_all < 0.01 AND af_exac_all < 0.01))
-                                                     AND filter IS NULL\""),
+                                                      AND (aaf < 0.1 AND aaf_esp_all < 0.01 AND
+                                                      aaf_1kg_all < 0.01 AND af_exac_all < 0.01))
+                                                      AND filter IS NULL \" --gt-filter \"(gt_types).(family_id== \'", 
+                                                      family_name, "\').(!=HOM_REF).(count>=1)\""),
                                          test_name = 'ACMG59')
 
 # rbindlist will collapse each element of the list into one data frame
 # gemini_query_wrapper() and gemini_test_wrapper() will add the test name
 # to each query, so you can distinguish them later
-GEMINI_data <- rbindlist(GEMINI_list)
+GEMINI_data <- rbindlist(GEMINI_list, fill = TRUE)
 save(GEMINI_data, file=output_df)
 
