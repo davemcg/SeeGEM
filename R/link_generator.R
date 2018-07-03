84b598bd2ac7db00baffb28d96c6ae741e114f20
David McGaughey <mcgaughey@gmail.com>
2018-07-03 08:20:58 -0400

    comments added, added logic to break link_name on delimiter

--- R/link_generator.R
+++ R/link_generator.R
@@ -9,7 +9,7 @@
 #' @param split_on What delimiter to split the field on? Defaults to comma.
 #' @param link_name What to use for the link name? Defaults to using the row value of the column
 #'
-#'@return None
+#' @return None
 #'
 #' @export
 #'
@@ -28,14 +28,21 @@ link_generator <- function(base_url, ID, split_on=',', link_name = NA){
   else {
     items <- strsplit(ID, split_on)[[1]]
     url <- ''
-    for (single_item in items){
+    # many links have sub-structure (link ClinVar IDs with the |)
+    # the strsplit above will break on the delimiter and create multiple links
+    for (i in seq_along(items)){
       if (is.na(link_name)){
-        url <- paste(paste0('<a href="', base_url, single_item, '" target="_blank">', single_item, '</a> '), url)
+        url <- paste(paste0('<a href="', base_url, items[i], '" target="_blank">', items[i], '</a> '), url)
       }
       else{
-        url <- paste(paste0('<a href="', base_url, single_item, '" target="_blank">', link_name, '</a> '), url)
+        # check if the link_name also has delimiters like the ID
+        if (!is.na(strsplit(link_name, split_on)[[1]][i])){
+          link_name = strsplit(link_name, split_on)[[1]][i]
+        }
+        url <- paste(paste0('<a href="', base_url, items[i], '" target="_blank">', link_name, '</a> '), url)
       }
     }
+    # yet another check for missing data
     if (ID == 'None' | ID == '-1') {
       url <- NA
     }
