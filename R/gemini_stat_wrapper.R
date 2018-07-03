c5c333d475f6b6da0aa514c74f02670ac825d06d
David McGaughey <mcgaughey@gmail.com>
2018-07-03 08:31:23 -0400

    put the actual system calls in, added some comments, fixed a list() typo

--- R/gemini_stat_wrapper.R
+++ R/gemini_stat_wrapper.R
@@ -6,7 +6,9 @@
 #' 
 #' @param gemini_db is the name of your GEMINI database (with path, if necessary)
 #' @param ID is the family name or sample name to pull stats from
-#' @param is_family Defaults to `yes`. If this is a sample name, change to 'no'
+#' @param is_family Defaults to `yes`. The GEMINI databases's ped will be pulled 
+#' to find the sample names associated with the family_id. If this is a sample 
+#' name, change to 'no'.  
 #' 
 #' @return None
 #'
@@ -18,8 +20,9 @@
 gemini_stat_wrapper <- function(gemini_db, ID, is_family = 'yes'){
   gemini_stats = list()
   if (is_family == 'yes'){
-    ped_tmp <-  paste0("/tmp/ped", sample(1e6:2e6,1))
+    ped_tmp <- paste0("/tmp/ped", sample(1e6:2e6,1))
     ped_query <- paste("gemini query --header -q \"SELECT * FROM SAMPLES\"", gemini_db, ">", ped_tmp)
+    system(ped_query)
     ped <- read_tsv(ped_tmp)
     ID <- ped %>% filter(family_id == ID) %>% pull(name)
     # gemini query --header -q 'select * from samples' 2018_06_28__OGVFB_exomes.GATK.PED_master.gemini.db
@@ -29,9 +32,10 @@ gemini_stat_wrapper <- function(gemini_db, ID, is_family = 'yes'){
   # gemini stats --gts-by-sample 2018_06_28__OGVFB_exomes.GATK.PED_master.gemini.db
   gemini_stats_tmp <-  paste0('/tmp/gem_stats_', sample(1e6:2e6,1))
   gemini_stats_query <- paste("gemini stats --gts-by-sample", gemini_db, ">", gemini_stats_tmp)
+  system(gemini_stats_query)
   gemini_stats <- read_tsv(gemini_stats_tmp)
   
-  out <- list
+  out <- list()
   out$ID <- ID
   out$gemini_stats <- gemini_stats
   out
